8329ea9dbd4c08d3ef77d163be0cdd12a526bb3f
dEnis-ponti <delavaifag@gmail.com>
2022-12-30 07:43:54 +0300

    nightFever for Products

--- cuvava/functions.php
+++ cuvava/functions.php
@@ -139,9 +139,10 @@ add_action( 'widgets_init', 'cuvava_widgets_init' );
  */
 function cuvava_scripts() {
 	wp_enqueue_style( 'cuvava-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'cuvava-general-style', get_template_directory_uri() . "/assets/css/general.css", array(), _S_VERSION );
 	wp_style_add_data( 'cuvava-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( '.cuvava-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'cuvava-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -175,4 +176,8 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+// Подключаем функции WC
+require get_template_directory() . '/inc/woocommerce-functions.php';
+
 
