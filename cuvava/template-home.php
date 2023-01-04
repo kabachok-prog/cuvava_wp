913484b598458225d5a91d979a5d889eaf86158d
dEnis-ponti <delavaifag@gmail.com>
2023-01-04 20:20:43 +0300

    sync from hosting 04.01 by Denis

---
+++ cuvava/template-home.php
@@ -0,0 +1,206 @@
+<?php
+/**
+ * Template name: Homepage Template
+ */
+
+get_header(); ?>
+
+
+<section style="max-width: 1120px;">
+        <div class="first-section">
+            <div class="first-text">
+                <h1>Medical Marijuana Card Online</h1>
+                <p>Telemedicine Platform Physicians from MDsimple offer online consultations and evaluations to determine if the patient is eligible for the Medical Marijuana Card. These licensed professionals provide health and medical information for each client and data.</p>
+                <select name="state" onchange="_go()" id="select-state">
+                    <option value="1" selected disabled>Select Your State</option>
+                    <option value="/pages/blog.html">2</option>
+                    <option value="3">3</option>
+                </select>
+            </div>
+            <img src="<?php bloginfo('template_url') ?>/assets/img/weedhome.png" alt="">
+        </div>
+    </section>
+    <section style="max-width: 1179px;">
+        <h2 class="title-how">How it work</h2>
+        <div class="cards__content">
+            <div class="card">
+                <div class="circle">1</div>
+                <img src="<?php bloginfo('template_url') ?>/assets/img/doche.png" alt="">
+                <div class="text-title">Book your appointment</div>
+                <div class="text-subtitle">Provide some basic medical history and book an appointment with a licensed MMJ doctor. The fee includes your consultation fee, physician copay, and the medical card evaluation.</div>
+            </div>
+            <div class="card">
+                <div class="circle">2</div>
+                <img src="<?php bloginfo('template_url') ?>/assets/img/videodoctor.png" alt="">
+                <div class="text-title">Talk to a MMJ Doctor</div>
+                <div class="text-subtitle">Consult with a pain management physician who will evaluate your health and medical ailments and answer any questions you have about medical treatment. This can be either online through your phone it’s depending on where you live to get medical card (MMJ).</div>
+            </div>
+            <div class="card">
+                <div class="circle">3</div>
+                <img src="<?php bloginfo('template_url') ?>/assets/img/mariju.png" alt="">
+                <div class="text-title">Receive a medical card</div>
+                <div class="text-subtitle">Health and medical If you are approved for medical card,you’ll receive your recommendation after you make medical pain management doctor visit Depending on your state, you will either be granted immediate access to dispensaries or wait to receive your card in the mail.</div>
+            </div>
+        </div>
+    </section>
+    <section style="max-width: 986px;">
+        <div class="block__services">
+            <div class="service">
+                <img src="<?php bloginfo('template_url') ?>/assets/img/pricing.png" alt="">
+                <div class="title">Pricing:</div>
+                <div class="subtitle">At most $199 annually</div>
+            </div>
+            <div class="service grande">
+                <img src="<?php bloginfo('template_url') ?>/assets/img/customer.png" alt="">
+                <div class="title">Customer Service:</div>
+                <div class="subtitle">Available 24/7 whenever <br>
+                    you need us</div>
+            </div>
+            <div class="service">
+                <img src="<?php bloginfo('template_url') ?>/assets/img/guarant.png" alt="">
+                <div class="title">Our Guarantee:</div>
+                <div class="subtitle">Approved or your <br>
+                    money back</div>
+            </div>
+        </div>
+    </section>
+    <section style="width: 100%; background-color: #F6F6F6; margin-top: 50px;">
+        <div class="video__content">
+            <div class="content__text">
+                <div class="video__text-title">
+                    Promotion Video
+                </div>
+                <div class="video__text-subtitle">
+                    Medical Card Online
+                </div>
+                <select name="state" id="select-state">
+                    <option value="1" selected disabled>Start Now</option>
+                    <option value="2"></option>
+                    <option value="3"></option>
+                </select>
+            </div>
+            <div class="content__video">
+                <iframe title="vimeo-player" src="https://player.vimeo.com/video/687257747?h=832c17d7fd" width="373" height="345" frameborder="0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </section>
+    <section style="max-width: 1159px;">
+        <div class="doctor__content">
+            <div class="doctor__text">
+                <div class="doctor__text-title">
+                    Get Your Medical Marijuana Card <br>
+                    in Your State <br>
+                    Online for <span style="color: #1C833E;">$149</span>
+                </div>
+                <a href="#" style="text-decoration: none;">
+                    <div class="start-now">
+                    Select Your State
+                    </div>
+                </a>
+            </div>
+            <div class="doctor__photo">
+                <img src="<?php bloginfo('template_url') ?>/assets/img/doctor.png" alt="">
+            </div>
+        </div>
+    </section>
+    <section style="max-width: 1159px;">
+        <div class="local__dispensaries">
+            <div class="dispensaries__header">
+                <div class="dispensaries__header-text">
+                    <div class="dispensaries__header-title">
+                        Local dispensaries
+                    </div>
+                    <div class="dispensaries__header-subtitle">
+                        Order online from our sponsored partners.
+                    </div>
+                </div>
+                <div class="dispensaries__header-link">
+                    <a href="#">See all dispensaries</a>
+                </div>
+            </div>
+            <div class="dispensaries__cards">
+                <div class="dispensaries__card">
+                    <img src="<?php bloginfo('template_url') ?>/assets/img/card1.png" alt="">
+                    <div class="card-name">
+                        Name Dispansaries
+                    </div>
+                    <div class="card-info">
+                        Dispansaries Info 
+                    </div>
+                    <a href="#" style="text-decoration: none;">
+                        <div class="start-now">
+                            See info
+                        </div>
+                    </a>
+                </div>
+                <div class="dispensaries__card">
+                    <img src="<?php bloginfo('template_url') ?>/assets/img/card2.png" alt="">
+                    <div class="card-name">
+                        Name Dispansaries
+                    </div>
+                    <div class="card-info">
+                        Dispansaries Info 
+                    </div>
+                    <a href="#" style="text-decoration: none;">
+                        <div class="start-now">
+                            See info
+                        </div>
+                    </a>
+                </div>
+                <div class="dispensaries__card">
+                    <img src="<?php bloginfo('template_url') ?>/assets/img/card3.png" alt="">
+                    <div class="card-name">
+                        Name Dispansaries
+                    </div>
+                    <div class="card-info">
+                        Dispansaries Info 
+                    </div>
+                    <a href="#" style="text-decoration: none;">
+                        <div class="start-now">
+                            See info
+                        </div>
+                    </a>
+                </div>
+                <div class="dispensaries__card">
+                    <img src="<?php bloginfo('template_url') ?>/assets/img/card4.png" alt="">
+                    <div class="card-name">
+                        Name Dispansaries
+                    </div>
+                    <div class="card-info">
+                        Dispansaries Info 
+                    </div>
+                    <a href="#" style="text-decoration: none;">
+                        <div class="start-now">
+                            See info
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section style="max-width: 1120px">
+        <div class="card__on__way">
+            <div class="way__title">
+                Your card is on it’s way!
+            </div>
+            <div class="way__subtitle">
+                After a doctor ultimately approves you for MMJ on our platform, health and medical you will quickly be registered with your respective state’s program. Once this is completed, you’ll either be granted immediate access to your local dispensaries or wait for a physical card to come in the mail medical card. Every state is a little different, we’ll make sure to guide you through every step of the process. Our states New York NY, Virginia VA, Michigan MI, Florida FL, Connecticut CT, Pennsylvania PA, Washington DC
+            </div>
+            <a href="#" style="text-decoration: none; ">
+                <div class="find-out">
+                    Find Out More
+                </div>
+            </a>
+        </div>
+    </section>
+    <section style="max-width: 1132px;">
+        <div class="reviews__content">
+            <div class="reviews-title">
+                Reviews
+            </div>
+        </div>
+    </section>
+
+
+
+<?php get_footer();
